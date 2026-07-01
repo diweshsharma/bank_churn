94a423bb3ed2b6a1829c83a8f59cb6e77793506a
diweshsharma <diweshsharma.janu@gmail.com>
2026-07-01 09:01:11 +0530

    preprocess

---
+++ preprocessing.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder , LabelEncoder
+from sklearn.compose import ColumnTransformer
+
+numerical = ['CreditScore' , 'Age' ,'Tenure' ,'Balance' , 'NumOfProducts' ,'EstimatedSalary' , 'Satisfaction Score' ,'Point Earned'
+    
+]
+
+labelcat = ['Gender']
+
+onehotcat =['Geography' , 'Card Type'
+    
+]
+
+def preprocess():
+    return ColumnTransformer(
+        transformers = [(
+            'encoder' ,
+            OneHotEncoder(handle_unknown = 'ignore'),
+            onehotcat ) ,
+                        
+            ('encoder2', LabelEncoder(), labelcat),
+            
+            ('scaler' , StandardScaler(), numerical)
+        ]
+    )
